01eba07e7832ec3df87c1215e38f8a409f94373f
hashimlirustam <125773354+hashimlirustam@users.noreply.github.com>
2023-06-15 20:34:32 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        info rustam = new info();
+        rustam.setName("Rustam");
+        rustam.setBalance(1500);
+        rustam.setUsername("rustam");
+        rustam.setPassword("admin");
+
+        info orkhan = new info();
+        orkhan.setName("Orkhan");
+        orkhan.setBalance(1800);
+        orkhan.setUsername("orkhan");
+        orkhan.setPassword("admin");
+
+        ArrayList<info> array = new ArrayList<>();
+        array.add(rustam);
+        array.add(orkhan);
+
+        for (info info : array) {
+            System.out.println("Hello dear user . Please login !");
+            Scanner scanner = new Scanner(System.in);
+                System.out.println("Username:");
+                String user = scanner.next();
+                System.out.println("Password:");
+                String pass = scanner.next();
+                if (info.getUsername().equals(user) && info.getPassword().equals(pass)) {
+                    System.out.println(info.getName());
+                    operationBalance(info.getUsername(), info.getName(), info.getBalance());
+                    break;
+                } else {
+                    System.out.println("Your information is false . Please try again !");
+                    main(args);
+                    break;
+                }
+            }
+        }
+    public static void operationBalance(String username ,String name, int balance) {
+        System.out.println("Choose yor operation type :\n1.Show balance\n2.Cash in\n3.Cash out\n4.Contact with bank\n5.Exit system");
+        Scanner operation = new Scanner(System.in);
+        int op = operation.nextInt();
+        switch (op) {
+            case 1:
+               showBalance(username,name,balance);
+                break;
+            case 2:
+                cashIn(username, name, balance);
+                break;
+            case 3:
+                cashOut(username,name,balance);
+                break;
+            case 4 :
+                System.out.println("Phone : +994555555555\n Email : hashimlirustam@gmail.com\nContinue -> 1  | Exit -> 2");
+                break;
+            case 5:System.out.println("You are exitting system. Have a good day!");
+            break;
+        }
+    }
+
+    private static void showBalance(String username ,String name, int balance) {
+        System.out.println("Your balance is " + balance+ "\nContinue -> 1  | Exit -> 2");
+        Scanner cont = new Scanner(System.in);
+        int cnt = cont.nextInt();
+        switch (cnt){
+            case 1: operationBalance(username, name , balance);
+            break;
+            case 2:
+                System.out.println("Thanks for operation! Have a good day!");
+        }
+    }
+
+    private static void cashOut(String username , String name ,int balance) {
+        System.out.println("Enter ammount :");
+        Scanner out = new Scanner(System.in);
+        int outAm = out.nextInt();
+        if (outAm<balance){
+        balance= balance -outAm;
+        System.out.println("Your balance is " + balance +" now.\nContinue -> 1  | Exit -> 2");
+        int cnt = out.nextInt();
+        System.out.println("Continue -> 1  | Exit -> 2");
+        switch (cnt){
+            case 1: operationBalance(username , name , balance);
+            break;
+            case 2:
+                System.out.println("Thanks for operation! Have a good day!");
+                break;
+        }
+        return;}
+        else {
+            System.out.println("Your balance isn`t enough!");
+            System.out.println("Continue -> 1  | Exit -> 2");
+            int cnt = out.nextInt();
+            switch (cnt){
+                case 1: operationBalance(username , name , balance);
+                    break;
+                case 2:
+                    System.out.println("Thanks for operation! Have a good day!");
+                    break;
+            }
+        }
+    }
+
+    private static void cashIn(String username,String name,int balance ) {
+        System.out.println("Enter ammount :");
+        Scanner in = new Scanner(System.in);
+        int inAm= in.nextInt();
+        System.out.println("hELLO");
+        balance = balance+inAm;
+        System.out.println("Your balance is " + balance +" now.\nContinue -> 1  | Exit -> 2");
+        int cnt = in.nextInt();
+        switch (cnt){
+            case 1: operationBalance(username , name , balance);
+                break;
+            case 2:
+                System.out.println("Thanks for operation! Have a good day!");
+                break;
+        }
+        return;
+    }
+
+}
+
+
---
+++ src/info.java
@@ -0,0 +1,48 @@
+
+
+public class info {
+    private String username;
+    private String password;
+    private int balance;
+    private String name;
+
+    public info() {
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+    private String username() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    private int balance() {
+        return this.balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
